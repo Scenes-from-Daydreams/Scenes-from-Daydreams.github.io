a6b4f1203d6d54b912f6cbd316487d3337f2280e
Jeff Blake <jtblake@middlebury.edu>
2024-08-29 19:54:02 -0400

    Please

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
