c9489220112ad6b138e66baaa2e4772c01a09c87
Jeff Blake <jtblake@middlebury.edu>
2024-09-06 13:42:19 -0400

    Moved images to public

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { searchForWorkspaceRoot } from "vite";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	server: {
+		fs: {
+		  allow: [
+			// search up for workspace root
+			searchForWorkspaceRoot(process.cwd()),
+			// your custom rules
+			'/path/to/custom/allow',
+			'./public/images/'
+		  ],
+		},
+	  },
 });
